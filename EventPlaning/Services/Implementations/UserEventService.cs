5e434f0cc5e666c64e5791a4ef7236128a28b429
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 09:06:22 +0300

    feat:add IUserEvent service imp

---
+++ EventPlaning/Services/Implementations/UserEventService.cs
@@ -0,0 +1,39 @@
+﻿using EventPlaning.Data.Repository;
+using EventPlaning.Models;
+using EventPlaning.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace EventPlaning.Services.Implementations
+{
+    public class UserEventService : IUserEventService
+    {
+        private IRepository<UserEvent> _userEvent;
+        public UserEventService(IRepository<UserEvent> userEvent)
+        {
+            _userEvent = userEvent ?? throw new ArgumentNullException(nameof(userEvent));
+        }
+        public void AddItem(UserEvent newUserEvent, string userId)
+        {
+            newUserEvent.Id = Guid.NewGuid();
+            newUserEvent.Owner = Guid.Parse(userId);
+
+            _userEvent.AddItem(newUserEvent);
+            Save();
+        }
+
+        public UserEvent GetItemById(Guid Id)
+        {
+            return _userEvent.GetItemById(Id);
+        }
+
+        public IEnumerable<UserEvent> GetItems()
+        {
+            return _userEvent.GetItems();
+        }
+        public void Save()
+        {
+            _userEvent.Save();
+        }
+    }
+}
