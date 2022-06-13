cf8879fca69c5ee427d6752bacf2d6a6bddae429
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 09:00:19 +0300

    feat:add Repository pattern

---
+++ EventPlaning/Data/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventPlaning.Data.Repository
+{
+    public interface IRepository<T> : IDisposable
+    {
+        IEnumerable<T> GetItems();
+        T GetItemById(Guid itemId);
+        void AddItem(T item);
+        void Save();
+    }
+}
