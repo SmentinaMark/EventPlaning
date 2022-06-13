1b2651378fad51661c7b87b6916a21707f86f128
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 09:05:30 +0300

    feat:add IUserEvent service interface

---
+++ EventPlaning/Services/Interfaces/IUserEventService.cs
@@ -0,0 +1,14 @@
+﻿using EventPlaning.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EventPlaning.Services.Interfaces
+{
+    public interface IUserEventService
+    {
+        IEnumerable<UserEvent> GetItems();
+        UserEvent GetItemById(Guid Id);
+        void AddItem(UserEvent newUserEvent, string userId);
+        void Save();
+    }
+}
