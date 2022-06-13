be13935cbcd29732302769468c56c309d61cc84a
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 09:05:20 +0300

    feat:add IParticipant service interface

---
+++ EventPlaning/Services/Interfaces/IParticipantService.cs
@@ -0,0 +1,10 @@
+﻿using EventPlaning.Models;
+using System;
+
+namespace EventPlaning.Services.Interfaces
+{
+    public interface IParticipantService
+    {
+        void GoToEvent(Guid eventId, UserEvent userEvent, string userId);
+    }
+}
