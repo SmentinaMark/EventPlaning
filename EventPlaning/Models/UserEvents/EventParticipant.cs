f13f3410b04f6bfeceff2abc59baefcf18d55a50
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 09:02:10 +0300

    feat:add UserEvents models

---
+++ EventPlaning/Models/UserEvents/EventParticipant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EventPlaning.Models.UserEvents
+{
+    public class EventParticipant
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+
+        public Guid UserEventId { get; set; }
+        public UserEvent? UserEvent { get; set; }
+    }
+}
---
+++ EventPlaning/Models/UserEvents/UserEvent.cs
@@ -0,0 +1,16 @@
+﻿using EventPlaning.Models.UserEvents;
+using System;
+using System.Collections.Generic;
+
+namespace EventPlaning.Models
+{
+    public class UserEvent
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Guid Owner { get; set; }
+        public List<EventParticipant> Participants { get; set; }
+    }
+}
