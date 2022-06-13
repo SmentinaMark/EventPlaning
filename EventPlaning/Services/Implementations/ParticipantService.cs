cba91cfe7c9f7c93abe101599d036615df67674f
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 09:06:07 +0300

    feat:add IParticipant service imp

---
+++ EventPlaning/Services/Implementations/ParticipantService.cs
@@ -0,0 +1,24 @@
+﻿using EventPlaning.Data.Repository;
+using EventPlaning.Models;
+using EventPlaning.Models.UserEvents;
+using EventPlaning.Services.Interfaces;
+using System;
+
+namespace EventPlaning.Services.Implementations
+{
+    public class ParticipantService : IParticipantService
+    {
+        private IRepository<EventParticipant> _participant;
+        public ParticipantService(IRepository<EventParticipant> participant)
+        {
+            _participant = participant ?? throw new ArgumentNullException(nameof(participant));
+        }
+
+        public void GoToEvent(Guid eventId, UserEvent userEvent, string userId)
+        {
+            EventParticipant newParticipant = new EventParticipant { Id = Guid.NewGuid(), UserEvent = userEvent, UserEventId = userEvent.Id, UserId = Guid.Parse(userId) };
+            _participant.AddItem(newParticipant);
+            _participant.Save();
+        }
+    }
+}
