4ce3421f4d260d99a1060430f0faac5dd85dc770
MarkSmentina <mark.smentina@bk.ru>
2022-06-13 08:59:46 +0300

    feat:add ApplicationContext

---
+++ EventPlaning/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EventPlaning.Models;
+using EventPlaning.Models.UserEvents;
+using EventPlaning.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlaning.Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<UserEvent> UserEvents { get; set; }
+        public DbSet<EventParticipant> EventPartisipiants { get; set; }
+        public DbSet<User> User { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
